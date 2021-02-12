ad29899ed747f521332943843061bdf5b9828cee
Corey Hall <coreyhall008@gmail.com>
2021-02-12 10:35:44 -0500

    Adding Player1 & 2

--- public/main.js
+++ public/main.js
@@ -1,7 +1,26 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+function updatePlayer1() {
+  const player1 = document.querySelector('input.enteredName1').value
+  document.querySelector('h2.Player1Name').textContent = player1
+  console.log(player1)
 }
+function updatePlayer2() {
+  const player2 = document.querySelector('input.enteredName2').value
+  document.querySelector('h2.Player2Name').textContent = player2
+  console.log(player2)
+}
+
+function main() {}
+if (document.querySelector('h2.Player1Name')) {
+  document.querySelector('h2.Player1Name').textContent = 'Player 1'
+}
+if (document.querySelector('h2.Player2Name')) {
+  document.querySelector('h2.Player2Name').textContent = 'Player 2'
+}
+document
+  .querySelector('button.enter-name1')
+  .addEventListener('click', updatePlayer1)
+document
+  .querySelector('button.enter-name1')
+  .addEventListener('click', updatePlayer2)
 
 document.addEventListener('DOMContentLoaded', main)
