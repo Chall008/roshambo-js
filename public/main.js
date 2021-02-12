04466d739034bbc1ed45b2c3e32d1858d04abe05
Corey Hall <coreyhall008@gmail.com>
2021-02-12 16:19:34 -0500

    Added the function for p1 &p2

--- public/main.js
+++ public/main.js
@@ -1,65 +1,100 @@
-// let wins = {
-//   rock:['scissors'],
-//   paper:['rock'],
-//   scissors:['paper'],
-// }
+let player1Choice = ''
+let player2Choice = ''
 
-function p1ChoiceRock() {
-  // if (choice === '#Player1Rock')
+function p1ChoosesRock() {
+  player1Choice = 'rock'
+  console.log(player1Choice)
 }
-function p1ChoicePaper() {}
-function p1ChoiceRock() {}
-// function p1ChoiceLizard(){
-
-// }
-// function p2ChoiceSpock(){
-
-// }
-function p2ChoiceRock() {}
-function p2ChoicePaper() {}
-function p2ChoiceScissor() {}
+function p1ChoosesPaper() {
+  player1Choice = 'paper'
+  console.log(player1Choice)
+}
+function p1ChoosesScissors() {
+  player1Choice = 'scissors'
+  console.log(player1Choice)
+}
+// function p1ChoiceLizard(){}
+// function p2ChoiceSpock(){}
+function p2ChoosesRock() {
+  player2Choice = 'rock'
+  console.log(player2Choice)
+}
+function p2ChoosesPaper() {
+  player2Choice = 'paper'
+  console.log(player2Choice)
+}
+function p2ChoosesScissor() {
+  player2Choice = 'scissors'
+  console.log(player2Choice)
+}
 // function p2ChoiceLizard()
 // function p2ChoiceSpock()
 
 function updatePlayer1() {
-  const player1 = document.querySelector('input#enteredName1').value
-  document.querySelector('h2#Player1Name').textContent = player1
+  const player1 = document.getElementById('enteredName1').value
+  document.getElementById('Player1Name').textContent = player1
   console.log(player1)
 }
 function updatePlayer2() {
-  const player2 = document.querySelector('input#enteredName2').value
-  document.querySelector('h2#Player2Name').textContent = player2
+  const player2 = document.getElementById('enteredName2').value
+  document.getElementById('Player2Name').textContent = player2
   console.log(player2)
 }
+// function replay() {
+//   location.reload(true)
+// }
 
-function main() {}
-if (document.querySelector('h2#Player1Name')) {
-  document.querySelector('h2#Player1Name').textContent = 'Player 1'
+function main() {
+  const p1RockChoice = document.getElementById('Player1Rock')
+  p1RockChoice.addEventListener('click', p1ChoosesRock)
+
+  const p1PaperChoice = document.getElementById('Player1Paper')
+  p1PaperChoice.addEventListener('click', p1ChoosesPaper)
+
+  const p1ScissorChoice = document.getElementById('Player1Scissors')
+  p1ScissorChoice.addEventListener('click', p1ChoosesScissors)
+
+  const p2RockChoice = document.getElementById('Player2Rock')
+  p2RockChoice.addEventListener('click', p2ChoosesRock)
+
+  const p2PaperChoice = document.getElementById('Player2Paper')
+  p2PaperChoice.addEventListener('click', p2ChoosesPaper)
+
+  const p2ScissorChoice = document.getElementById('Player2Scissors')
+  p2ScissorChoice.addEventListener('click', p2ChoosesScissor)
+
+  if (document.getElementById('Player1Name')) {
+    document.getElementById('Player1Name').textContent = 'Player 1'
+  }
+  if (document.getElementById('Player2Name')) {
+    document.getElementById('Player2Name').textContent = 'Player 2'
+  }
+  document
+    .getElementById('enter-name1')
+    .addEventListener('click', updatePlayer1)
+  document
+    .getElementById('enter-name2')
+    .addEventListener('click', updatePlayer2)
+
+  // document.querySelector('').addEventListener('click')
+  // document.querySelector('').addEventListener('click')
+  // document.querySelector('').addEventListener('click')
+  // document.querySelector('').addEventListener('click')
+  // document
+  // .querySelector('')
+  // .addEventListener('click', )
+
+  // document.querySelector('').addEventListener('click')
+  // document.querySelector('').addEventListener('click')
+  // document.querySelector('').addEventListener('click')
+  // document.querySelector('').addEventListener('click')
+  // document
+  // .querySelector('')
+  // .addEventListener('click', )
+
+  // const replayButton = document
+  //   .querySelector('#Replay')
+  //   .addEventListener('click', replay)
 }
-if (document.querySelector('h2#Player2Name')) {
-  document.querySelector('h2#Player2Name').textContent = 'Player 2'
-}
-document
-  .querySelector('button#enter-name1')
-  .addEventListener('click', updatePlayer1)
-document
-  .querySelector('button#enter-name2')
-  .addEventListener('click', updatePlayer2)
-
-document.querySelector('').addEventListener('click')
-document.querySelector('').addEventListener('click')
-document.querySelector('').addEventListener('click')
-document.querySelector('').addEventListener('click')
-// document
-// .querySelector('')
-// .addEventListener('click', )
-
-document.querySelector('').addEventListener('click')
-document.querySelector('').addEventListener('click')
-document.querySelector('').addEventListener('click')
-document.querySelector('').addEventListener('click')
-// document
-// .querySelector('')
-// .addEventListener('click', )
 
 document.addEventListener('DOMContentLoaded', main)
